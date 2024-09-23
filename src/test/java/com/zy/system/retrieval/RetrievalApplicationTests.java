208a339efce5bfe745030ff98f7602d6c65019af
zhouyuan <1969300201@qq.com>
2024-09-23 13:54:16 +0800

    feat(*):后端项目初始化

---
+++ src/test/java/com/zy/system/retrieval/RetrievalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.system.retrieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrievalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
